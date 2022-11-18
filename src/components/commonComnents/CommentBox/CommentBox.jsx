991640714e620f44d5cd37597a325860dd84efe1
Ayesha Ashraf <ayesha.ashraf@zweidevs.com>
2022-11-18 19:29:20 +0500

    Commentbox: clean code

--- src/components/commonComnents/CommentBox/CommentBox.jsx
+++ src/components/commonComnents/CommentBox/CommentBox.jsx
@@ -6,43 +6,6 @@ import "react-comments-section/dist/index.css";
 import "./CommentBox.css";
 
 const CommentComponent = (props) => {
-  const data = [
-    {
-      userId: "01a",
-      comId: "012",
-      fullName: "Riya Negi",
-      avatarUrl: "https://ui-avatars.com/api/name=Riya&background=random",
-      userProfile: "https://www.linkedin.com/in/riya-negi-8879631a9/",
-      text: "Hey, Loved your blog! ",
-      replies: [
-        {
-          userId: "02a",
-          comId: "013",
-          userProfile: "https://www.linkedin.com/in/riya-negi-8879631a9/",
-          fullName: "Adam Scott",
-          avatarUrl: "https://ui-avatars.com/api/name=Adam&background=random",
-          text: "Thanks! It took me 1 month to finish this project but I am glad it helped out someone!🥰",
-        },
-        {
-          userId: "01a",
-          comId: "014",
-          userProfile: "https://www.linkedin.com/in/riya-negi-8879631a9/",
-          fullName: "Riya Negi",
-          avatarUrl: "https://ui-avatars.com/api/name=Riya&background=random",
-          text: "thanks!😊",
-        },
-      ],
-    },
-    {
-      userId: "02b",
-      comId: "017",
-      fullName: "Lily",
-      userProfile: "https://www.linkedin.com/in/riya-negi-8879631a9/",
-      text: "I have a doubt about the 4th point🤔",
-      avatarUrl: "https://ui-avatars.com/api/name=Lily&background=random",
-      replies: [],
-    },
-  ];
 
   const postComment = async (comment) => {
     const response = await postAPI("userSongComment" , {
@@ -50,11 +13,6 @@ const CommentComponent = (props) => {
       song_id:props.songID,
       comment: comment
     });
-    console.log("======data of comment" ,{
-      user_id: getID(),
-      song_id:props.songID,
-      comment: comment
-    } )
     console.log("============response of comment" , response)
     if (response.status == 200) {
     }
